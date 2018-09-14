e415108a57068b35a52f9057bb342d15976c03e9
Giacomo Barletti <giacomo.barletti@kyklos-group.com>
2018-09-14 12:37:04 +0200

    modifiche ad ALE Valorizzazione + scroll in consutivo e report

--- ReportWeb.Models/JSON/ALEValorizzaJson.cs
+++ ReportWeb.Models/JSON/ALEValorizzaJson.cs
@@ -16,7 +16,28 @@ namespace ReportWeb.Models.JSON
         [DataMember(Name = "Prezzo")]
         public decimal? Prezzo{ get; set; }
 
+        [DataMember(Name = "Costo")]
+        public decimal? Costo { get; set; }
+
         [DataMember(Name = "Nota")]
         public string Nota { get; set; }
+
+        [DataMember(Name = "CostiFase")]
+        public CostoFaseJson[] CostiFase { get; set; }
+    }
+
+
+    [DataContract]
+    public class CostoFaseJson
+    {
+        [DataMember(Name = "IdAleDettaglio")]
+        public decimal IdAleDettaglio { get; set; }
+
+        [DataMember(Name = "Fase")]
+        public string Fase { get; set; }
+
+        [DataMember(Name = "Costo")]
+        public decimal? Costo { get; set; }
+
     }
 }
