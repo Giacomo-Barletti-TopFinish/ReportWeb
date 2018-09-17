da9b574ac3482fd216a4a5af5150f1e34102c4d8
Giacomo Barletti <giacomo.barletti@kyklos-group.com>
2018-09-17 15:26:27 +0200

    ALE pulizia codice

--- ReportWeb.Models/ALE/GruppiModel.cs
+++ ReportWeb.Models/ALE/GruppiModel.cs
@@ -30,41 +30,5 @@ namespace ReportWeb.Models.ALE
         public bool ValorizzazioneAnnulabile { get; set; }
         public bool ApprovazioneAnnulabile { get; set; }
     }
-
-    public class GruppoValorizzatoModel
-    {
-        public List<ValorizzatoModel> Dettagli { get; set; }
-        public string NotaValorizzazione { get; set; }
-        public string LavoranteCodice { get; set; }
-        public string LavoranteDescrizione { get; set; }
-        public bool Aperto { get; set; }
-        public decimal IDALEGRUPPO { get; set; }
-        public bool ValorizzazioneAnnullabile { get; set; }
-        public decimal ValoreTotale{ get; set; }
-    }
-
-    public class GruppoApprovatoModel
-    {
-        public List<ApprovatoModel> Dettagli { get; set; }
-        public string NotaApprovazione { get; set; }
-        public string LavoranteCodice { get; set; }
-        public string LavoranteDescrizione { get; set; }
-        public bool Aperto { get; set; }
-        public decimal IDALEGRUPPO { get; set; }
-        public bool ApprovazioneAnnullabile { get; set; }
-        public decimal ValoreTotale { get; set; }
-        public decimal ValoreApprovatoTotale { get; set; }
-    }
-
-    public class GruppoFatturatoModel
-    {
-        public List<FatturatoModel> Dettagli { get; set; }
-        public string NotaFatturazione { get; set; }
-        public string LavoranteCodice { get; set; }
-        public string LavoranteDescrizione { get; set; }
-        public bool Aperto { get; set; }
-        public decimal IDALEGRUPPO { get; set; }
-        public bool FatturazioneAnnullabile { get; set; }
-        public decimal ValoreApprovatoTotale { get; set; }
-    }
+   
 }
