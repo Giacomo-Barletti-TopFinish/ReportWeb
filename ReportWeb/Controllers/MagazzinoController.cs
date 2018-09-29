34e84ec066f2ce4f7bf1b4ab6d37c0c91f1b8efe
VIAMATTEI\giacomo.barletti <g.barletti@topfinish.it>
2018-09-29 11:15:18 +0200

    Aggiunto Magazzino Controller

---
+++ ReportWeb/Controllers/MagazzinoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ReportWeb.Controllers
+{
+    public class MagazzinoController : Controller
+    {
+        // GET: Magazzino
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
